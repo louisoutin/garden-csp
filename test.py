1eb1761d37a6515e5a9be15a9f697a1517b59217
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-24 14:00:44 +0100

    test

--- test.py
+++ test.py
@@ -1,47 +1,53 @@
-carrot = {
-    "shape_id": 1,
+import numpy as np
+
+carrot = 1
+tomato = 2
+
+carrot_shape = {
+    "shape_id": carrot,
     "shape_width": 4,
     "shape_height": 2
 }
 
-tomato = {
-    "shape_id": 2,
+tomato_shape = {
+    "shape_id": tomato,
     "shape_width": 5,
     "shape_height": 3
 }
 
-vegetables = [
+vegetable_shapes = [
     {},
-    carrot,  # carrot id: 1
-    tomato  # tomato id: 2
+    carrot_shape,  # carrot id: 1
+    tomato_shape  # tomato id: 2
 ]
 
 
 def init_matrix(cols, rows):
-    arr = [[0] * cols] * rows
-    return arr
+    return np.zeros((rows, cols), dtype=np.uint8)
 
 
-def display_matrix(arr):
-    for row in arr:
-        for col in row:
-            print(col, end=" ")
+def display_matrix(arr, cols, rows):
+    for i in range(rows):
+        for j in range(cols):
+            print(arr[i][j], end=" ")
         print()
 
 
-def place_shape(arr, id, top_left_i, top_left_j):
-    shape_width = vegetables[id]["shape_width"]
-    shape_height = vegetables[id]["shape_height"]
-    print(shape_width, shape_height)
+def place_shape(arr, shape_id, top_left_i, top_left_j):
+    shape_width = vegetable_shapes[shape_id]["shape_width"]
+    shape_height = vegetable_shapes[shape_id]["shape_height"]
     for i in range(top_left_i, top_left_i + shape_width):
         for j in range(top_left_j, top_left_j + shape_height):
-            arr[i][j] = id
+            arr[j][i] = shape_id
 
 
 width = 20
 height = 10
 garden = init_matrix(width, height)
-display_matrix(garden)
+display_matrix(garden, width, height)
 print()
-place_shape(garden, 1, 0, 0)
-display_matrix(garden)
+place_shape(garden, carrot, 0, 0)
+place_shape(garden, tomato, 0, 4)
+place_shape(garden, tomato, 10, 7)
+
+display_matrix(garden, width, height)
