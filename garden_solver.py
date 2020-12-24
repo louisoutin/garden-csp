70fd35893c7b105142ed34fe883c661c3aa3c0ac
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2020-12-24 10:48:04 +0100

    Update garden_solver.py

--- garden_solver.py
+++ garden_solver.py
@@ -4,6 +4,10 @@ from constraint import Problem, BacktrackingSolver
 class GardenSolver:
     def __init__(self):
         self.problem = Problem(BacktrackingSolver())
+        self.add_constraints()
+
+    def add_constraints(self):
+        return
 
     def solve(self):
         return self.problem.getSolutions()
